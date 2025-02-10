c0c129ba333f31e6e3e8a768785c81c0b45a08cf
Robert Luiz <robertlsc2016@gmail.com>
2025-02-10 01:07:38 -0300

    fix: improve error handling in storeController and filterStores

--- src/controllers/storeController.ts
+++ src/controllers/storeController.ts
@@ -128,7 +128,7 @@ export class StoreController {
       res.status(200).json(store_by_location);
     } catch (err: any) {
       logger.log("error", err)
-      res.status(500).json({ message: err.message });
+      res.status(err.status).json(err);
     }
   }
 }
--- src/utils/filterStores.ts
+++ src/utils/filterStores.ts
@@ -1,43 +1,50 @@
 export const filterStores = async ({ stores }: { stores: any }) => {
-  const filteredStores = stores.map(
-    ({
-      store_id,
-      name,
-      store_type,
-      image,
-      sub_group,
-      parent_store_type,
-    }: any) => ({
-      store_id,
-      store_name: name,
-      store_type: store_type,
-      store_img: image,
-      sub_group: sub_group,
-      parent_store_type: parent_store_type,
-    })
-  );
+  try {
+    const filteredStores = stores.map(
+      ({
+        store_id,
+        name,
+        store_type,
+        image,
+        sub_group,
+        parent_store_type,
+      }: any) => ({
+        store_id,
+        store_name: name,
+        store_type: store_type,
+        store_img: image,
+        sub_group: sub_group,
+        parent_store_type: parent_store_type,
+      })
+    );
 
-  const groupMapping = {
-    turbo: "turbo",
-    mercados: "super",
-    express: "express",
-    shopping: "hogar",
-    farmacias: "farmacia",
-    especializadas: "especializada",
-    bebidas: "licores",
-    presentes: "regalos",
-    flores: "floristeria",
+    const groupMapping = {
+      turbo: "turbo",
+      mercados: "super",
+      express: "express",
+      shopping: "hogar",
+      farmacias: "farmacia",
+      especializadas: "especializada",
+      bebidas: "licores",
+      presentes: "regalos",
+      flores: "floristeria",
+    };
 
-  };
+    const stores_by_group = Object.fromEntries(
+      Object.entries(groupMapping).map(([key, value]) => [
+        key,
+        filteredStores.filter(
+          (store: any) => store.sub_group.toLowerCase() === value
+        ),
+      ])
+    );
 
-  const stores_by_group = Object.fromEntries(
-    Object.entries(groupMapping).map(([key, value]) => [
-      key,
-      filteredStores.filter(
-        (store: any) => store.sub_group.toLowerCase() === value
-      ),
-    ])
-  );
-
-  return stores_by_group;
+    return stores_by_group;
+  } catch (err: any) {
+    throw {
+      message: "erro ao limpar dados fornecidos pela api",
+      status: 503,
+      error: err,
+    };
+  }
 };
