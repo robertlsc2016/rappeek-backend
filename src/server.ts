aa3c1c6f04efdd04eeefdd9b91c512fb2f27a73d
Robert Luiz <robertlsc2016@gmail.com>
2025-02-16 18:10:49 -0300

    chore: remove listen 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`API documentation: http://localhost:${PORT}/api-docs`);
   console.log(`API rodando na porta http://localhost:${PORT}`);
 });
