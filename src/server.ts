77878b62145b52dcae0d20fd4c1decf823ebb3f1
Robert Luiz <robertlsc2016@gmail.com>
2025-02-16 18:13:28 -0300

    chore: add PORT env

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import app from ".";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT);
+
 app.listen(PORT, "127.0.0.1", () => {
   console.log(`API documentation: http://localhost:${PORT}/api-docs`);
   console.log(`API rodando na porta http://localhost:${PORT}`);
